9e6163fd62ffc3827edc9b356a7ddc566e845842
Scott Baker <smbaker@gmail.com>
2021-01-10 11:20:03 -0800

    reorg the speech dict

---
+++ python/phrases.py
@@ -0,0 +1,12 @@
+from herolisting_speech import *
+from smbaker_speech import *
+
+PHRASES =  {"daisy": SPK_DAISY_DAISY,
+            "america": SPK_AMERICA_THE_BEAUTIFUL,
+            "jack": SPK_JACK_BE_NIMBLE,
+            "mary": SPK_MARY_HAD_A_LITTLE_LAMB,
+            "muffet": SPK_LITTLE_MISS_MUFFET,
+            "iamrobot": SPK_I_AM_YOUR_PERSONAL_ROBOT,
+            "brain": SPK_I_HAVE_A_BRAIN_JUST_LIKE_YOU_DO,
+            
+            "leia": HELLO_LEIA,}
